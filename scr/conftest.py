2a1ddb86ef20b8680c18de2f3fa5fa25a376d86b
Regin <43794933+hateworldwide@users.noreply.github.com>
2026-02-12 22:21:29 +0300

    Revert "yaml hw"

--- scr/conftest.py
+++ scr/conftest.py
@@ -41,57 +41,13 @@ def pytest_addoption(parser):
         default="192.168.1.110",
         help="This request url"
     )
-    parser.addoption(
-        "--remote",
-        action="store_true",
-        help="Run tests on Selenoid"
-    )
-    parser.addoption(
-        "--browser_version",
-        default="128.0",
-        help="Browser version for remote"
-    )
-    parser.addoption(
-        "--executor",
-        default=None,
-        help="Executor: selenoid or local"
-    )
-    parser.addoption(
-        "--selenoid_url",
-        default="http://localhost:4444/wd/hub",
-        help="Selenoid URL"
-    )
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("--browser")
     base_url = request.config.getoption("--url")
-    remote = request.config.getoption("--remote")
-    browser_version = request.config.getoption("--browser_version")
-    executor = request.config.getoption("--executor")
-    selenoid_url = request.config.getoption("--selenoid_url")
 
-    driver = None
-
-    if remote or executor == "selenoid":
-
-        if browser_name == "ch":
-            options = webdriver.ChromeOptions()
-        else:
-            options = webdriver.FirefoxOptions()
-
-        options.set_capability("browserName", browser_name if browser_name in ("chrome", "firefox", "edge") else "chrome")
-        options.set_capability("browserVersion", browser_version)
-        options.set_capability("selenoid:options", {
-            "enableVNC": True,
-            "enableVideo": False
-        })
-
-        driver = webdriver.Remote(
-            command_executor=selenoid_url,
-            options=options
-        )
-    elif browser_name == "chrome":
+    if browser_name == "chrome":
         options = ChromeOps()
         options.add_argument('--headless=new')
         options.add_argument('--disable-dev-shm-usage')
